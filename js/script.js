47a21bda3c591a4cb5959518e0ea7e9fb323b562
idigrow/ytj <wnaj55@idigrow.com>
2024-12-13 16:06:06 +0900

    1213

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 $(function(){
 	$(document).ready(function() {
 		$('#fullpage').fullpage({
-			//options here
 			autoScrolling:true,
 			scrollHorizontally: true,
 			navigation: false,
 			scrollingSpeed: 500,
 			fitToSection: true,
 			lockAnchors: true,
-			anchors:['first', 'second', 'third', 'four'],
+			anchors: ['section1', 'section2', 'section3', 'section4'],
+			onLeave:function(index, nextIndex, direction) {
+				if(index == 1 && direction == 'down') {
+					$('header').css('display','block');
+				} else if (index == 2 && direction == 'up') {
+					$('header').css('display','none');
+				} 
+			}
 		});
 
 	});
