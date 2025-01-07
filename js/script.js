5db30f15b95865fe0ae7485a984a09bfda40cd2a
yoontaejong <yoontaejoung@naver.com>
2025-01-07 18:01:50 +0900

    0107

--- js/script.js
+++ js/script.js
@@ -10,8 +10,10 @@ $(function(){
 			anchors: ['section1', 'section2', 'section3', 'section4'],
 			onLeave:function(index, nextIndex, direction) {
 				if(index == 1 && direction == 'down') {
+					$('.header-seciton').css('display','none');
 					$('header').css('display','block');
 				} else if (index == 2 && direction == 'up') {
+					$('.header-seciton').css('display','flex');
 					$('header').css('display','none');
 				} 
 			}
