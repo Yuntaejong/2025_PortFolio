2b2c1814c50df079feb9155bd8146d77b9ab091f
yoontaejong <yoontaejoung@naver.com>
2025-02-11 16:46:51 +0900

    0211

--- js/script.js
+++ js/script.js
@@ -10,6 +10,192 @@ $(function(){
 				var $menuItems = $('#menu li');
 				$menuItems.removeClass('active');
 				$menuItems.filter('[data-menuanchor="section' + nextIndex + '"]').addClass('active');
+
+
+				if (index == 3 && direction == 'down') {
+					$('.p1').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section4 .work-line').css({
+						"height":"400px"
+					})
+				} else if (index == 4 && direction == 'up') {
+					$('.p1').css({
+						"width":"0px",
+						"height":"0px"
+					});
+					$('.section4 .work-line').css({
+						"height":"0px"
+					})
+				}
+
+				if (index == 4 && direction == 'down') {
+					$('.p1').css({
+						"width":"0px",
+						"height":"0px"
+					});
+					$('.section4 .work-line').css({
+						"height":"0px"
+					})
+
+					$('.p2').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section5 .work-line').css({
+						"height":"400px"
+					})
+
+				} else if (index == 5 && direction == 'up') {
+					$('.p1').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section4 .work-line').css({
+						"height":"400px"
+					})
+
+					$('.p2').css({
+						"width":"0px",
+						"height":"0px"
+					});
+					$('.section5 .work-line').css({
+						"height":"0px"
+					})
+				}
+
+				if (index == 5 && direction == 'down') {
+					$('.p2').css({
+						"width":"0px",
+						"height":"0px"
+					});
+					$('.section5 .work-line').css({
+						"height":"0px"
+					})
+
+					$('.p3').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section6 .work-line').css({
+						"height":"400px"
+					})
+
+				} else if (index == 6 && direction == 'up') {
+					$('.p2').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section5 .work-line').css({
+						"height":"400px"
+					})
+
+					$('.p3').css({
+						"width":"0px",
+						"height":"0px"
+					});
+					$('.section6 .work-line').css({
+						"height":"0px"
+					})
+				}
+
+				if (index == 6 && direction == 'down') {
+					$('.p3').css({
+						"width":"0px",
+						"height":"0px"
+					});
+					$('.section5 .work-line').css({
+						"height":"0px"
+					})
+
+					$('.p4').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section7 .work-line').css({
+						"height":"400px"
+					})
+
+				} else if (index == 7 && direction == 'up') {
+					$('.p3').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section5 .work-line').css({
+						"height":"400px"
+					})
+
+					$('.p4').css({
+						"width":"0px",
+						"height":"0px"
+					});
+					$('.section7 .work-line').css({
+						"height":"0px"
+					})
+				}
+
+				if (index == 7 && direction == 'down') {
+					$('.p4').css({
+						"width":"0px",
+						"height":"0px"
+					});
+					$('.section6 .work-line').css({
+						"height":"0px"
+					})
+
+					$('.p5').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section8 .work-line').css({
+						"height":"400px"
+					})
+
+				} else if (index == 8 && direction == 'up') {
+					$('.p4').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section6 .work-line').css({
+						"height":"400px"
+					})
+
+					$('.p5').css({
+						"width":"0px",
+						"height":"0px"
+					});
+					$('.section8 .work-line').css({
+						"height":"0px"
+					})
+				}
+
+				if (index == 8 && direction == 'down') {
+					$('.p5').css({
+						"width":"0px",
+						"height":"0px"
+					});
+					$('.section7 .work-line').css({
+						"height":"0px"
+					})
+
+					$('.p6').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section9 .work-line').css({
+						"height":"400px"
+					})
+
+				} else if (index == 9 && direction == 'up') {
+					$('.p5').css({
+						"width":"1500px",
+						"height":"1500px"
+					});
+					$('.section7 .work-line').css({
+						"height":"400px"
+					})
+				}
 			}
 		});
 		
